3c5f3afc244d3a99a3806b43d7f1b78116b69006
cicababba <cicababba1@gmail.com>
2022-07-14 18:33:17 +0200

    update

--- QCommerce/src/main/java/q/bit/qcommerce/service/CleanInactiveUsersService.java
+++ QCommerce/src/main/java/q/bit/qcommerce/service/CleanInactiveUsersService.java
@@ -12,10 +12,15 @@ public class CleanInactiveUsersService {
     @Autowired
     private UserService userService;
 
+    @Value("${inactive.users.cron.active}")
+    private boolean active;
+
     private static final Logger log = Logger.getLogger(CleanInactiveUsersService.class);
 
     @Scheduled(cron = "${delete.inactive.users.cron}")
     public void cleanInactiveUsers() {
+        if(!active)
+            return;
         log.info("start cleaning inactive users");
         userService.cleanInactiveUsers();
         log.info("finished cleaning inactive users");
